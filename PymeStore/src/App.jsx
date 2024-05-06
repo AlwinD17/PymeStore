d000180c3519622e1579938cbce1f1637fa71f83
AlwinD <edudavilaraffo@gmail.com>
2024-05-05 23:49:54 -0500

    First pages

--- PymeStore/src/App.jsx
+++ PymeStore/src/App.jsx
@@ -3,10 +3,29 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<HomeLayout />,
+    children: [
+      {
+        index: true,
+        element: <Landign />,
+      },
+      {
+        path:"login",
+        element:<login />, 
+      }
+    ]
+  }
+])
+
 function App() {
 
   return (
     <>
+      <RouterProvider router={router} />
+      <ToastContainer position="top-center" />
     </>
   )
 }
---
+++ PymeStore/src/pages/HomeLayout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomeLayout = () =>{
+    return(
+    <>
+    </>)
+}
+
+export default HomeLayout
