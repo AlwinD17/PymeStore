711ae39f580c423f47fd54a575a6ec2ff1844661
AlwinD <edudavilaraffo@gmail.com>
2024-05-06 19:01:30 -0500

    Paginas adicionales

--- PymeStore/src/App.jsx
+++ PymeStore/src/App.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
-import { HomeLayout, Login, Landing, Register } from './pages'
+import { HomeLayout, Login, Landing, Register, Additional } from './pages'
 
 const router = createBrowserRouter([
   {
@@ -22,6 +22,16 @@ const router = createBrowserRouter([
         path: '/register',
         element: <Register />,
       },
+      {
+        name: 'about us',
+        path: '/about_us',
+        element: <Additional title={{name : 'Sobre nosotros'}} />,
+      },
+      {
+        name: 'Términos y Condiciones',
+        path: '/terminos_condiciones',
+        element: <Additional title=  {{name:'Términos y condiciones'}} />,
+      }
     ],
   },
 ]);
